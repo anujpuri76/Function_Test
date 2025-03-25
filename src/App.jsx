b8cfe92af5e35f1ce818fa513b7f982113de31a8
ANUJ PURI <purianuj603@gmail.com>
2025-03-25 12:15:59 +0530

    ye mera first commit hai

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import './App.css'
 const App = () => {
- const data = [
-  {
-    id: 1,
-    name:"Parwinder",
-    age:40,
-  },
-  {
-    id:2,
-    name:"Dharender",
-    age:70,
-  },
-  
+  const data = [
+    {
+      id: 1,
+      name: "Parwinder",
+      age: 40,
+    },
+    {
+      id: 2,
+      name: "Dharender",
+      age: 70,
+    },
 
- ]
+
+  ]
 
   return (
     <>
-   
-      
+
+
     </>
   );
 }
