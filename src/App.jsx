fc1bb84da0181ad4e8048c8043ef0cb31dce1646
ANUJ PURI <purianuj603@gmail.com>
2025-03-25 12:03:13 +0530

    name dharender was changed

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+ const data = [
+  {
+    id: 1,
+    name:"Parwinder",
+    age:40,
+  },
+  {
+    id:2,
+    name:"Dharender",
+    age:70,
+  },
+  
+
+ ]
+
+  return (
+    <>
+   
+      
+    </>
+  );
+}
+
+export default App
